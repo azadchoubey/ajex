d75a7b4229d2690560abc6d083fed01b30a647db
Azad Choubey <48096790+azadchoubey@users.noreply.github.com>
2021-07-14 22:33:25 +0530

    Add files via upload

--- login.php
+++ login.php
@@ -10,38 +10,66 @@
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo base_url() ?>css/login.css">
 </head>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js">
 </script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 <body>
     <div class="container">
         <div class="row">
             <div class="col-6">
+                <span class="msg"></span>
                 <?php
 echo 'Username'. form_input(['name' => 'Username','class'=>'form-control','id'=>'Username']); 
 echo 'Password'. form_input(['name' => 'Password    ','class'=>'form-control','id'=>'Password']); 
-echo form_submit(['class'=>'btn btn-success mt-3','value'=>"Submit",'id'=>'Submit'])
+echo form_submit(['class'=>'btn btn-success mt-3','value'=>"Submit",'id'=>'Submit','name' =>'Submit'])
     ?>
             </div>
         </div>
     </div>
 
 
-
-
-
-
-
     <script>
     $(document).ready(function() {
         $('#Submit').click(function() {
             var username = $('#Username').val();
-            var pssword = $('#Password').val();
+            var password = $('#Password').val();
 
-            if (username == " " || pssword == "") {
-              
-                    $('#Username').css({'border:3px solid red'} )
+            if (username == " " || password == "") {
+
+                $('#Username').css({
+                    "border": "3px solid red"
+                });
+                $('#Password').css({
+                    "border": "3px solid red"
+                });
+                $('.msg').html('All Fields Are Requied<br>');
+            } else {
+
+                $.ajax({
+                    type: "POST",
+                    url: '<?php echo base_url('index.php/public/restapi/getlogin'); ?>',
+                    data: {
+                        username: username,
+                        password: password
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                     
+                        console.log(data);
+
+                        if ('success'== true ) {
+                            sessionStorage.setItem(userid, 'Value');
+                            window.location.href =
+                                "<?php echo base_url('index.php/public/restapi/getlogin/afterlogin'); ?>";
+                        } else
+                        $('.msg').html('Username or Password is incorrect<br>');
+
+
+                    }
+
+                });
+
             }
 
 
